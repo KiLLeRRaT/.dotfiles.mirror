0dd8ca0b87757968d04e6dbe23e99dbb36e42436
Albert Gouws <albert@gouws.org>
2026-09-03 09:15:04 +1200

    install(fzf): Update to latest version

--- install/install-fzf-from-tar.sh
+++ install/install-fzf-from-tar.sh
@@ -4,7 +4,8 @@ set -e
 mkdir /tmp/fzf
 pushd /tmp/fzf
 
-curl -L -o fzf.tar.gz https://github.com/junegunn/fzf/releases/download/v0.62.0/fzf-0.62.0-linux_amd64.tar.gz
+# curl -L -o fzf.tar.gz https://github.com/junegunn/fzf/releases/download/v0.62.0/fzf-0.62.0-linux_amd64.tar.gz
+curl -L -o fzf.tar.gz https://github.com/junegunn/fzf/releases/download/v0.74.3/fzf-0.74.3-linux_amd64.tar.gz
 tar -xzf fzf.tar.gz
 sudo cp fzf /usr/local/bin
 rm -rf fzf fzf.tar.gz
